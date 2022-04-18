51d2a0ed312c8664b7c14d3d03bb2598717e90d5
Cristian Alcober <cristian_alcober@outlook.com>
2022-04-18 11:10:56 -0300

    BaSP-2022: ex01_variables_and_operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,27 @@
+// Exercise 1 - a
+var variableOne = 10;
+var variableTwo = 5;
+var variableThree = variableOne + variableTwo;
+
+console.log('Variable 1 =', variableOne);
+console.log('Variable 2 =', variableTwo);
+console.log('Variable 1 + Variable 2 =', variableThree);
+
+// Exercise 1 - b
+var phraseOne = 'Hello';
+var phraseTwo = 'World';
+var phraseCompleted = phraseOne + ' ' + phraseTwo;
+
+console.log('Phrase 1 is: ' + phraseOne);
+console.log('Phrase 2 is: ' + phraseTwo);
+console.log('Combining them will result in: ' + phraseCompleted);
+
+// Exercise 1 - c
+var longOne = phraseOne.length; 
+var longTwo = phraseTwo.length;
+var longComplete = longOne + longTwo;
+
+console.log('Phrase 1 is: ' + phraseOne + ' and it has ' + longOne + ' characters.');
+console.log('Phrase 2 is: ' + phraseTwo + ' and it has ' + longTwo + ' characters.');
+console.log('The combination of this phrases would have ' + longComplete + ' characters');
+
